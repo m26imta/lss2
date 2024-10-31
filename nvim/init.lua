3d4cc5dc54fe0a4d466869c37d00e8e716f0bcda
admin <admin@local>
2024-10-31 17:30:56 +0700

    update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,11 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+PLUGINS_TBL = {}
+function import(item)
+  table.insert(PLUGINS_TBL, { import = item })
+end
+--
+-- add LazyVim and import its plugins
+table.insert(PLUGINS_TBL, { "LazyVim/LazyVim", import = "lazyvim.plugins" })
+-- import/override with your plugins
+import("plugins")
+
 require("config.lazy")
