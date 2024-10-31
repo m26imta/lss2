d27aeaf947b1d0f6d4abf2ab278ec849313e6fc1
admin <admin@local>
2024-10-31 17:40:38 +0700

    add support for vscode

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,7 +5,16 @@ end
 --
 -- add LazyVim and import its plugins
 table.insert(PLUGINS_TBL, { "LazyVim/LazyVim", import = "lazyvim.plugins" })
--- import/override with your plugins
-import("plugins")
+
+if vim.g.vscode then
+  -- config & plugins for vscode
+  -- https://github.com/vscode-neovim/vscode-neovim
+  import("lazyvim.plugins.extras.vscode")
+  import("vscode_neovim.plugins")
+  require("vscode_neovim.config.options")
+else
+  -- Normal LazyVim in neovim
+  import("plugins")
+end
 
 require("config.lazy")
